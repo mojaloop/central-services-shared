f4b3e10514f85350e19c1b91f294b75c5d48991f
Rajiv Mothilal <rajivmothilal@gmail.com>
2019-01-16 11:17:21 +0200

    Feature/perf log to file (#29)
    * update logging to handle file creation else storing it in working directory path. updated test and version
    
    * updated version correctly
    
    * updated env variable to log_file_path_name

--- src/logger.js
+++ src/logger.js
@@ -2,27 +2,46 @@
 
 const winston = require('winston')
 
-const level = process.env.LOG_LEVEL || 'info'
+let Logger
 
-const transportConsole = new winston.transports.Console({ json: false, timestamp: true, prettyPrint: true, colorize: true, level: level })
+if (!Logger) {
+  const level = process.env.LOG_LEVEL || 'info'
+  const filename = process.env.LOG_FILE_PATH_NAME || 'logs/combined.log'
+  const transportFile = new winston.transports.File({
+    json: false,
+    timestamp: true,
+    prettyPrint: true,
+    colorize: true,
+    filename: filename,
+    level: level
+  })
 
-const Logger = new (winston.Logger)({
-  levels: {
-    error: 0,
-    warn: 1,
-    info: 2,
-    perf: 3,
-    verbose: 4,
-    debug: 5,
-    silly: 6
-  },
-  transports: [
-    transportConsole
-  ],
-  exceptionHandlers: [
-    transportConsole
-  ],
-  exitOnError: false
-})
+  Logger = winston.createLogger({
+    format: winston.format.combine(
+      winston.format.colorize({all: true}),
+      winston.format.timestamp({
+        format: 'YYYY-MM-dd\'T\'HH:mm:ss.SSSZ'
+      }),
+      winston.format.prettyPrint(),
+      winston.format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`)
+    ),
+    levels: {
+      error: 0,
+      warn: 1,
+      info: 2,
+      perf: 3,
+      verbose: 4,
+      debug: 5,
+      silly: 6
+    },
+    transports: [
+      transportFile
+    ],
+    exceptionHandlers: [
+      transportFile
+    ],
+    exitOnError: false
+  })
+}
 
 module.exports = Logger
