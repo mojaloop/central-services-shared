2de5693cd2a8ac73b64fb473537d915687cd0d57
Lewis Daly <lewis@vesselstech.com>
2019-07-19 10:09:17 +0800

    Added npm audit and license scanner to ci (#55)

--- test/errors/validation.test.js
+++ test/errors/validation.test.js
@@ -9,7 +9,7 @@ Test('ValidationError test', validationTest => {
     payloadTest.test('set error_id, message and validation_errors', test => {
       const message = 'some message'
       const validationErrors = [{}]
-      let error = new ValidationError(message, validationErrors)
+      const error = new ValidationError(message, validationErrors)
       test.equal(error.category, ErrorCategory.BAD_REQUEST)
       test.equal(error.payload.id, 'ValidationError')
       test.equal(error.payload.message, message)
