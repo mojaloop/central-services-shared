15fcb8f4402cdd44980f1893f340d000c073ce92
Rajiv Mothilal <rajivmothilal@gmail.com>
2019-08-01 11:51:52 +0200

    Feature/shared library #848 (#57)
    * added standard header transformers + enums for HTTP
    
    * fixed the module name
    
    * Updates to enums
    
    * Added unit tests
    
    - Moved util from ml-api and central-ledger
    - added unit tests for util + header.transformer.js
    - Added enums to Instanbul ignore list for coverage check as there is no actual functionality to check due to enums being only constant types.
    
    * Consolidated enums for events
    
    * changed clone function to use lodash deepClone method
    
    * random updates trying to fix unit tests
    
    * updates including kafka, enums hapi raw plugin and changed coverage tool and updated standards and dependencies
    
    * removed duplicate code
    
    * removed caret
    
    * updated version
    
    * added a unified util collection of functions
    removed errors folder and tests
    updated dependencies
    
    * removed unused dependencies
    
    * Added tests to improve coverage and added missing common enums
    
    * Updated according to changes required from ml-api-adapter
    fix broken tests
    
    * Updated to cater for more generic stream message creation.
    Ability to create metadata information now.
    Fixed broken tests
    
    * removed hard coded text
    
    * clean up obsolete functions and fix for endpoints request
    
    * removed bluebird dependency
    
    * fixing breaks in the code where converting an object to s string was a problem
    
    * snapshot version
    
    * fixed invalid case getting used on enum check
    
    * updated headers to align with PR https://github.com/mojaloop/ml-api-adapter/pull/104
    updated version
    
    * these are objects
    
    * version change for release
    
    * updated PR with changes according to Miguels review
    moved all message creation functionality to streaming/protocol
    
    * changes as per miguels comments
    moved general into util index.js
    moved kafka utility to kafka index.js
    moved breadcrumb from kafka to util
    version update of error dependency
    
    * name change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 'use strict'
 
 exports.Logger = require('./logger')
-exports.Encoding = require('./encoding')
-exports.BaseError = require('./errors/base')
-exports.ErrorCategory = require('./errors/category')
-exports.NotFoundError = require('./errors/not-found')
-exports.ValidationError = require('./errors/validation')
 exports.HealthCheck = require('./healthCheck/index')
+exports.Enum = require('./enums')
+exports.Util = require('./util')
