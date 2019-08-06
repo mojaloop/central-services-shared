fad9a24a98c5f18b029b4996870fdf11b70435c8
ggrg <georgi.georgiev@modusbox.com>
2019-08-06 16:12:02 +0300

    Resolve issues with src/index.js merge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 'use strict'
 
-exports.BaseError = require('./errors/base')
 exports.Comparators = require('./comparators/index')
-exports.Encoding = require('./encoding')
 exports.Enum = require('./enums')
-exports.ErrorCategory = require('./errors/category')
 exports.HealthCheck = require('./healthCheck/index')
 exports.Logger = require('./logger')
-exports.NotFoundError = require('./errors/not-found')
 exports.Util = require('./util')
-exports.ValidationError = require('./errors/validation')
