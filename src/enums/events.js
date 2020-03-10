f912c76d9af10eb5029858ad822cfbbd7604e02a
Miguel de Barros <miguel@debarros.me>
2020-03-10 13:00:22 +0000

    reverted prepare-position topic split

--- src/enums/events.js
+++ src/enums/events.js
@@ -67,8 +67,6 @@ const Event = {
     LIMIT_ADJUSTMENT: 'limit-adjustment',
     LOOKUP: 'lookup',
     POSITION: 'position',
-    POSITION_PREPARE: 'position-prepare',
-    POSITION_FULFIL: 'position-fulfil',
     PREPARE: 'prepare',
     PREPARE_DUPLICATE: 'prepare-duplicate',
     PROCESSING: 'processing',
