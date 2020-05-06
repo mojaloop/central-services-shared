dbd584e5ba45cbc688a4191310d688f6d0bc0a00
Shashi <shashikant.hirugade@modusbox.com>
2020-05-06 17:20:16 -0500

    added unit tests

--- test/unit/util/request.test.js
+++ test/unit/util/request.test.js
@@ -9,6 +9,36 @@ const proxyquire = require('proxyquire')
 const Enum = require('../../../src/enums')
 const Helper = require('../../util/helper')
 const Metrics = require('@mojaloop/central-services-metrics')
+const Uuid = require('uuid4')
+const JwsSigner = require('@mojaloop/sdk-standard-components').Jws.signer
+
+const signingKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEA0eJEh3Op5p6x137lRkAsvmEBbd32dbRChrCUItZbtxjf/qfB
+yD5k8Hn4n4vbqzP8XSGS0f6KmNC+iRaP74HVgzAqc4Uid4J8dtSBq3VmucYQYzLc
+101QjuvD+SKmZwlw/q0PtulmqlASI2SbMfwcAraMi6ab7v5W4EGNeIPLEIo3BXsQ
+DTCWqiZb7aXkHkcY7sOjAzK/2bNGYFmAthdYrHzvCkqnJ7LAHX3Oj7rJea5MqtuN
+B9POZYaD10n9JuYWdwPqLrw6/hVgPSFEy+ulrVbXf54ZH0dfMThAYRvFrT81yulk
+H95JhXWGdi6cTp6t8LVOKFhnNfxjWw0Jayj9xwIDAQABAoIBADB2u/Y/CgNbr5sg
+DRccqHhJdAgHkep59kadrYch0knEL6zg1clERxCUSYmlxNKSjXp/zyQ4T46b3PNQ
+x2m5pDDHxXWpT10jP1Q9G7gYwuCw0IXnb8EzdB+cZ0M28g+myXW1RoSo/nDjTlzn
+1UJEgb9Kocd5cFZOWocr+9vRKumlZULMsA8yiNwlAfJHcMBM7acsa3myCqVhLyWt
+4BQylVuLFa+A6QzpMXEwFCq8EOXf07gl1XVzC6LJ1fTa9gVM3N+YE+oEXKrsHCxG
+/ACgKsjepL27QjJ7qvecWPP0F2LxEZYOm5tbXaKJTobzQUJHgUokanZMhjYprDsZ
+zumLw9kCgYEA/DUWcnLeImlfq/EYdhejkl3J+WX3vhS23OqVgY1amu7CZzaai6vt
+H0TRc8Zsbi4jgmFDU8PFzytP6qz6Tgom4R736z6oBi7bjnGyN17/NSbf+DaRVcM6
+vnZr7jNC2FJlECmIN+dkwUA/YCr2SA7hxZXM9mIYSc+6+glDiIO5Cf0CgYEA1Qo/
+uQbVHhW+Cp8H0kdMuhwUbkBquRrxRZlXS1Vrf3f9me9JLUy9UPWb3y3sKVurG5+O
+SIlr4hDcZyXdE198MtDMhBIGqU9ORSjppJDNDVvtt+n2FD4XmWIU70vKBJBivX0+
+Bow6yduis+p12fuvpvpnKCz8UjOgOQJhLZ4GQBMCgYBP6gpozVjxkm4ML2LO2IKt
++CXtbo/nnOysZ3BkEoQpH4pd5gFmTF3gUJAFnVPyPZBm2abZvejJ0jGKbLELVVAo
+eQWZdssK2oIbSo9r2CAJmX3SSogWorvUafWdDoUZwlHfoylUfW+BhHgQYsyS3JRR
+ZTwCveZwTPA0FgdeFE7niQKBgQCHaD8+ZFhbCejDqXb4MXdUJ3rY5Lqwsq491YwF
+huKPn32iNNQnJcqCxclv3iln1Cr6oLx34Fig1KSyLv/IS32OcuY635Y6UPznumxe
+u+aJIjADIILXNOwdAplZy6s4oWkRFaSx1rmbCa3tew2zImTv1eJxR76MpOGmupt3
+uiQw3wKBgFjBT/aVKdBeHeP1rIHHldQV5QQxZNkc6D3qn/oAFcwpj9vcGfRjQWjO
+ARzXM2vUWEet4OVn3DXyOdaWFR1ppehz7rAWBiPgsMg4fjAusYb9Mft1GMxMzuwT
+Oyqsp6pzAWFrCD3JAoTLxClV+j5m+SXZ/ItD6ziGpl/h7DyayrFZ
+-----END RSA PRIVATE KEY-----`
 
 Test('ParticipantEndpoint Model Test', modelTest => {
   let sandbox
@@ -39,7 +69,7 @@ Test('ParticipantEndpoint Model Test', modelTest => {
     test.end()
   })
 
-  modelTest.test('getEndpoint should', async (getEndpointTest) => {
+  modelTest.test('sendRequest should', async (getEndpointTest) => {
     getEndpointTest.test('return the object of endpoints', async (test) => {
       const fsp = 'fsp'
       const requestOptions = {
@@ -148,6 +178,53 @@ Test('ParticipantEndpoint Model Test', modelTest => {
         test.ok(e instanceof Error)
         test.end()
       }
+    })
+
+    getEndpointTest.test('sign with JWS signature when JwsSigner object is passed', async (test) => {
+      const fsp = 'payerfsp'
+      const payeefsp = 'payeefsp'
+      const ID = Uuid()
+      const body = { errorInformation: { errorCode: 3106, errorDescription: 'Modified request' } }
+      const payload = JSON.stringify(body)
+      const requestOptions = {
+        url: Mustache.render(Config.ENDPOINT_SOURCE_URL + Enum.EndPoints.FspEndpointTemplates.TRANSFERS_PUT_ERROR, { fsp, ID }),
+        method: 'post',
+        headers: Helper.defaultHeaders(fsp, Enum.Http.HeaderResources.PARTICIPANTS, payeefsp)
+      }
+      const jwsSigner = new JwsSigner({
+        logger: null,
+        signingKey
+      })
+      request = sandbox.stub().returns({ status: 200 })
+      Model = proxyquire('../../../src/util/request', { axios: request })
+      const signSpy = Sinon.spy(jwsSigner, 'sign')
+      await Model.sendRequest(requestOptions.url, requestOptions.headers, Enum.Http.HeaderResources.SWITCH, fsp, requestOptions.method, payload, Enum.Http.ResponseTypes.JSON, undefined, jwsSigner)
+      test.ok(signSpy.calledOnce, 'JwsSigner.sign is called once')
+      test.ok('fspiop-signature' in signSpy.getCall(0).firstArg.headers, 'The header has fspiop-signature')
+      test.end()
+    })
+
+    getEndpointTest.test('NOT sign with JWS signature when JwsSigner object is NOT passed', async (test) => {
+      const fsp = 'payerfsp'
+      const payeefsp = 'payeefsp'
+      const ID = Uuid()
+      const body = { errorInformation: { errorCode: 3106, errorDescription: 'Modified request' } }
+      const payload = JSON.stringify(body)
+      const requestOptions = {
+        url: Mustache.render(Config.ENDPOINT_SOURCE_URL + Enum.EndPoints.FspEndpointTemplates.TRANSFERS_PUT_ERROR, { fsp, ID }),
+        method: 'post',
+        headers: Helper.defaultHeaders(fsp, Enum.Http.HeaderResources.PARTICIPANTS, payeefsp)
+      }
+      const jwsSigner = new JwsSigner({
+        logger: null,
+        signingKey
+      })
+      request = sandbox.stub().returns({ status: 200 })
+      Model = proxyquire('../../../src/util/request', { axios: request })
+      const signSpy = Sinon.spy(jwsSigner, 'sign')
+      await Model.sendRequest(requestOptions.url, requestOptions.headers, Enum.Http.HeaderResources.SWITCH, fsp, requestOptions.method, payload, Enum.Http.ResponseTypes.JSON, undefined, undefined)
+      test.equal(signSpy.callCount, 0, 'JwsSigner.sign is NOT called')
+      test.end()
     })
 
     getEndpointTest.end()
