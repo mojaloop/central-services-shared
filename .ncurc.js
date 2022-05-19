00c7fef72ced0688c0b522801e022cab7310f980
Miguel de Barros <miguel@debarros.me>
2022-05-19 12:18:59 +0200

    chore: renamed .ncurc from json to js so we can add comments

---
+++ .ncurc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reject: [
+    // TODO: Added "@hapi/catbox", "@hapi/catbox-memory" to ncurc for dep:check to ignore updates due to breaking changes which should be handled by another story
+    "@hapi/catbox",
+    "@hapi/catbox-memory"
+  ]
+}
